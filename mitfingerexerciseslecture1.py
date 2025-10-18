abbdcfeeaf51b2c38425519235ffd7c58d7a452a
Beril Titizoğlu <beriltitizoglucoding@gmail.com>
2025-10-18 23:35:48 +0300

    Retouch/testing

--- mitfingerexerciseslecture1.py
+++ mitfingerexerciseslecture1.py
@@ -2,8 +2,9 @@
 # Create a variable called total that adds a and b then multiplies the result by c.
 # Include a last line in your code to print the value: print(total)
 
-a = 5
-b = 10
-c = 2
+# a = 5
+# b = 10
+# c = 2
+
 total = (a + b) * c 
 print(total)
